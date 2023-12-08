3f6d364ddcf5be2e4266bae1123dd73174096c91
nicolamenale <nicola.menale001@studenti.uniparthenope.it>
2023-12-08 17:48:30 +0100

    Progetto TechWeb

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,45 +11,40 @@ AOS.init({
 
 });
 
-// Movement Animation to happen
-const cards = document.querySelectorAll(".card");
+//Movement Animation to happen
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
 
-// Loop through each card
-cards.forEach(card => {
-  // Items inside the card
-  const title = card.querySelector(".title");
-  const sneaker = card.querySelector(".sneaker img");
-  const purchase = card.querySelector(".purchase");
-  const description = card.querySelector(".info h3");
-  const sizes = card.querySelector(".sizes");
-
-  // Moving Animation Event
-  card.addEventListener("mousemove", (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-  });
-
-  // Animate In
-  card.addEventListener("mouseenter", (e) => {
-    card.style.transition = "none";
-    // Popout
-    title.style.transform = "translateZ(150px)";
-    sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
-    description.style.transform = "translateZ(125px)";
-    sizes.style.transform = "translateZ(100px)";
-    purchase.style.transform = "translateZ(75px)";
-  });
-
-  // Animate Out
-  card.addEventListener("mouseleave", (e) => {
-    card.style.transition = "all 0.5s ease";
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-    // Popback
-    title.style.transform = "translateZ(0px)";
-    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
-    description.style.transform = "translateZ(0px)";
-    sizes.style.transform = "translateZ(0px)";
-    purchase.style.transform = "translateZ(0px)";
-  });
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  description.style.transform = "translateZ(125px)";
+  sizes.style.transform = "translateZ(100px)";
+  purchase.style.transform = "translateZ(75px)";
+});
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
 });
