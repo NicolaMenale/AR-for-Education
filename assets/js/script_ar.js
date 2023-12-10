81ed21bd81d89e42cc991ba85afeb628e2ca4853
nicolamenale <nicola.menale001@studenti.uniparthenope.it>
2023-12-10 18:03:56 +0100

    Progetto TechWeb

--- assets/js/script_ar.js
+++ assets/js/script_ar.js
@@ -1,12 +1,31 @@
 var markers = document.querySelectorAll("a-marker");
+let         isMarkerVisible;
 markers.forEach(function(marker) {
     marker.addEventListener("markerFound", function (event) {
         console.log("Marker found: " + marker.id);
+        isMarkerVisible = true;
         window.parent.postMessage(marker.id, '*');
     });
 
     marker.addEventListener("markerLost", function (event) {
         console.log("Marker lost: " + marker.id);
+        isMarkerVisible = false;
         window.parent.postMessage(marker.id, '*');
     });
 });
+
+function handleScale(event) {
+    if (isMarkerVisible) {
+        this.scaleFactor *=
+            1 + event.detail.spreadChange / event.detail.startSpread;
+
+        this.scaleFactor = Math.min(
+            Math.max(this.scaleFactor, this.data.minScale),
+            this.data.maxScale
+        );
+
+        el.object3D.scale.x = scaleFactor * initialScale.x;
+        el.object3D.scale.y = scaleFactor * initialScale.y;
+        el.object3D.scale.z = scaleFactor * initialScale.z;
+    }
+}
